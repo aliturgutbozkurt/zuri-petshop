7f1ced3c3585679bc1b083f25c758557010eea02
Ali Turgut Bozkurt <aliturgutbozkurt@gmail.com>
2021-09-23 22:30:49 +0300

    initial files commit

---
+++ petshop-mobile/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Selam Zuri Petshop Team!</Text>
+      <Button
+          title="Giriş Yap"
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
