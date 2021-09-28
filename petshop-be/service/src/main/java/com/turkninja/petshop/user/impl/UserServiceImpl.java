4f165ed5df32b1a98c8aeabcdd63b108d716af58
moustaphastar <mustafayildiz1881@gmail.com>
2021-09-28 15:00:12 +0300

    Add service methods, pass granted authorities to User object.

--- petshop-be/service/src/main/java/com/turkninja/petshop/user/impl/UserServiceImpl.java
+++ petshop-be/service/src/main/java/com/turkninja/petshop/user/impl/UserServiceImpl.java
@@ -2,11 +2,23 @@ package com.turkninja.petshop.user.impl;
 
 import com.turkninja.petshop.UserRepository;
 import com.turkninja.petshop.UserRoleRepository;
+import com.turkninja.petshop.api.request.user.UserImageInsertRequest;
 import com.turkninja.petshop.api.request.user.UserSignupRequest;
+import com.turkninja.petshop.api.response.common.PageResponse;
+import com.turkninja.petshop.api.response.user.UserImageInsertResponse;
+import com.turkninja.petshop.api.response.user.UserResponse;
 import com.turkninja.petshop.entity.user.UserEntity;
 import com.turkninja.petshop.entity.user.UserRoleEntity;
+import com.turkninja.petshop.exception.AppMessage;
+import com.turkninja.petshop.exception.AppParameter;
+import com.turkninja.petshop.exception.ApplicationException;
 import com.turkninja.petshop.mapper.UserMapper;
 import com.turkninja.petshop.user.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,6 +26,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -35,7 +48,9 @@ public class UserServiceImpl implements UserService {
         Optional<UserEntity> userEntity = userRepository.findByEmail(email);
         if (!userEntity.isPresent()) throw new UsernameNotFoundException(email);
         UserEntity user = userEntity.get();
-        return new User(user.getEmail(), user.getPassword(), new ArrayList<>());
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName()));
+        return new User(user.getEmail(), user.getPassword(), authorities);
     }
 
     @Override
@@ -47,7 +62,56 @@ public class UserServiceImpl implements UserService {
         userRepository.save(userEntity);
     }
 
+    /**
+     * Finds the user with given id and returns a UserResponse object.
+     * @param id Id of the user.
+     * @return UserResponse representation of the found user.
+     * @throws ApplicationException
+     */
+    @Override
+    public UserResponse getOne(Long id) throws ApplicationException {
+        Optional<UserEntity> optionalUserEntity = userRepository
+                .findByIdAndActiveTrue(id);
+        if(!optionalUserEntity.isPresent()) {
+            throw new ApplicationException(AppMessage.RECORD_NOT_FOUND);
+        }
 
+        return userMapper.entityToUserResponse(optionalUserEntity.get());
+    }
+
+    /**
+     * Returns a paged response of users.
+     * @param page Page number.
+     * @param size Size of page.
+     * @return PageResponse representation of UserResponse.
+     * @throws ApplicationException
+     */
+    @Override
+    public PageResponse<UserResponse> list(int page, int size)
+            throws ApplicationException {
+        Page<UserEntity> entities = userRepository
+                .findAllByActiveTrue(PageRequest.of(page, size, Sort.Direction.DESC, "fullName"));
+        return userMapper.pageEntitiesToPageResponse(entities);
+    }
+
+    /**
+     * Deactivates a user.
+     * @param id Id of the user.
+     */
+    @Override
+    public void delete(Long id)
+            throws ApplicationException  {
+        UserEntity entity = userRepository.findByIdAndActiveTrue(id).orElseThrow(() ->
+                new ApplicationException(AppMessage.RECORD_NOT_FOUND,
+                        AppParameter.get("userId", id)));
+        entity.setActive(false);
+        userRepository.save(entity);
+    }
+
+    @Override
+    public UserImageInsertResponse insertUserImage(UserImageInsertRequest request) {
+        return new UserImageInsertResponse();
+    }
 
     private void setDefaultUserRole(UserEntity userEntity){
         Optional<UserRoleEntity> defaultUserRoleOptional = userRoleRepository.findById(1L);
