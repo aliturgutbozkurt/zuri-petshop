1f2890561c4698cd4f6c3ac53e21edbc943ed37a
moustaphastar <mustafayildiz1881@gmail.com>
2021-09-28 15:04:38 +0300

    Add list, user and delete endpoints with access rule.

--- petshop-be/web/src/main/java/com/turkninja/petshop/v1/UserResource.java
+++ petshop-be/web/src/main/java/com/turkninja/petshop/v1/UserResource.java
@@ -1,12 +1,21 @@
 package com.turkninja.petshop.v1;
 
 import com.turkninja.petshop.api.request.user.UserSignupRequest;
+import com.turkninja.petshop.api.response.common.PageResponse;
+import com.turkninja.petshop.api.response.user.UserResponse;
 import com.turkninja.petshop.user.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -25,5 +34,41 @@ public class UserResource {
     public ResponseEntity<?> signup(@Valid @RequestBody UserSignupRequest request) {
         userService.signUp(request);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public ResponseEntity<PageResponse<UserResponse>> pageList(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null
+                || auth
+                .getAuthorities()
+                .stream()
+                .anyMatch(r -> !r.getAuthority().equals("ROLE_ADMIN"))) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        PageResponse<UserResponse> pageResponse = userService.list(page, size);
+        return new ResponseEntity<>(pageResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable("id") Long id) {
+        UserResponse userResponse = userService.getOne(id);
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null
+                || auth
+                .getAuthorities()
+                .stream()
+                .anyMatch(r -> !r.getAuthority().equals("ADMIN"))) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
