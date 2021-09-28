25a2358559f69b3be46d82c10183d711c3544c89
moustaphastar <mustafayildiz1881@gmail.com>
2021-09-28 15:00:33 +0300

    Add new derived queries.

--- petshop-be/repository/src/main/java/com/turkninja/petshop/UserRepository.java
+++ petshop-be/repository/src/main/java/com/turkninja/petshop/UserRepository.java
@@ -3,9 +3,15 @@ package com.turkninja.petshop;
 
 import com.turkninja.petshop.base.ExtendedQueryDslJpaRepository;
 import com.turkninja.petshop.entity.user.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 
 import java.util.Optional;
 
 public interface UserRepository extends ExtendedQueryDslJpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
+    Page<UserEntity> findAllByActiveTrue(@PageableDefault() Pageable pageRequest);
+    Optional<UserEntity> findByIdAndActiveTrue(Long id);
+    Optional<UserEntity> findByIdAndActiveFalse(Long id);
 }
