bcd0b093452f6c3846f7a8c38aea10d3af69187c
moustaphastar <mustafayildiz1881@gmail.com>
2021-10-28 02:53:14 +0300

    Removed @Nullable annotation, improved regex pattern to fix unicode and case sensitivity problems, improved validation messages.

--- petshop-be/domain/src/main/java/com/turkninja/petshop/api/request/user/UserUpdateRequest.java
+++ petshop-be/domain/src/main/java/com/turkninja/petshop/api/request/user/UserUpdateRequest.java
@@ -2,7 +2,6 @@ package com.turkninja.petshop.api.request.user;
 
 import lombok.Data;
 
-import javax.annotation.Nullable;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -16,19 +15,19 @@ import javax.validation.constraints.Size;
 @Data
 public class UserUpdateRequest {
 
-    @Nullable
-    @Size(min = 2, max = 20, message = "İsim en az 2 en çok 20 karakter olmalıdır")
+    @Size(min = 2, max = 20,
+            message = "İsim en az 2 en çok 20 karakter olmalıdır")
     private String name;
 
-    @Nullable
-    @Size(min = 2, max = 25, message = "Soyad en az 2 en çok 25 karakter olmalıdır")
+    @Size(min = 2, max = 25,
+            message = "Soyad en az 2 en çok 25 karakter olmalıdır")
     private String lastName;
 
-    @Nullable
-    @Size(min = 10, max = 10, message = "Telefon numarasını doğru giriniz")
+    @Size(min = 10, max = 10,
+            message = "Telefon numarası 10 hane olmalıdır")
     private String mobilePhone;
 
-    @Nullable
-    @Pattern(regexp = ("MALE|FEMALE"), message = "MALE ya da FEMALE değerlerinden biri girilmelidir.")
+    @Pattern(regexp = ("(?i)e|k"),
+            message = "ERKEK veya KADIN değerlerinden biri girilmelidir")
     private String gender;
 }
