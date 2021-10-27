78b52eec53586805851e0f3a617ea40a2fe675f2
moustaphastar <mustafayildiz1881@gmail.com>
2021-10-27 21:57:56 +0300

    Implement update method.

--- petshop-be/service/src/main/java/com/turkninja/petshop/user/UserService.java
+++ petshop-be/service/src/main/java/com/turkninja/petshop/user/UserService.java
@@ -1,9 +1,8 @@
 package com.turkninja.petshop.user;
 
-import com.turkninja.petshop.api.request.user.UserImageInsertRequest;
 import com.turkninja.petshop.api.request.user.UserSignupRequest;
+import com.turkninja.petshop.api.request.user.UserUpdateRequest;
 import com.turkninja.petshop.api.response.common.PageResponse;
-import com.turkninja.petshop.api.response.user.UserImageInsertResponse;
 import com.turkninja.petshop.api.response.user.UserResponse;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -12,6 +11,6 @@ public interface UserService extends UserDetailsService {
     void signUp(UserSignupRequest request);
     PageResponse<UserResponse> list(int page, int size);
     UserResponse getOne(Long id);
+    UserResponse update(Long id, UserUpdateRequest userUpdateRequest);
     void delete(Long id);
-    UserImageInsertResponse insertUserImage(UserImageInsertRequest request);
 }
