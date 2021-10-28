4a069e2daf22bef1c96af5f9d0ed85ac8237db6d
moustaphastar <mustafayildiz1881@gmail.com>
2021-10-28 15:55:23 +0300

    Refactored to use patch mapping for update method.

--- petshop-be/web/src/main/java/com/turkninja/petshop/v1/UserResource.java
+++ petshop-be/web/src/main/java/com/turkninja/petshop/v1/UserResource.java
@@ -12,9 +12,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -56,7 +56,7 @@ public class UserResource {
         return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<UserResponse> update(
             @PathVariable("id") Long id,
             @Valid @RequestBody UserUpdateRequest request) {
