75d1aa8837c2d7ce1bd5a2d099fe3ed70be17654
moustaphastar <mustafayildiz1881@gmail.com>
2021-11-16 00:26:16 +0300

    Add method to find user roles by role name.

--- petshop-be/repository/src/main/java/com/turkninja/petshop/UserRoleRepository.java
+++ petshop-be/repository/src/main/java/com/turkninja/petshop/UserRoleRepository.java
@@ -3,5 +3,8 @@ package com.turkninja.petshop;
 import com.turkninja.petshop.base.ExtendedQueryDslJpaRepository;
 import com.turkninja.petshop.entity.user.UserRoleEntity;
 
+import java.util.Optional;
+
 public interface UserRoleRepository extends ExtendedQueryDslJpaRepository<UserRoleEntity, Long> {
+    Optional<UserRoleEntity> findByName(String name);
 }
