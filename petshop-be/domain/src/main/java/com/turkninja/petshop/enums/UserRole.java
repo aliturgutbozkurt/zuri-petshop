0ef1d1d1479551743a395aa08a5c6931fb5ba6c2
moustaphastar <mustafayildiz1881@gmail.com>
2021-11-16 00:01:27 +0300

    Add enumeration for application user roles

---
+++ petshop-be/domain/src/main/java/com/turkninja/petshop/enums/UserRole.java
@@ -0,0 +1,14 @@
+package com.turkninja.petshop.enums;
+
+/**
+ * Enumerated values for user roles.
+ *
+ * @author mstar
+ * @version 1.0
+ * @since 1.0, 2021-11-14
+ */
+public enum UserRole {
+    SUPER_ADMIN,
+    ADMIN,
+    USER
+}
